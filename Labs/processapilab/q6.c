56bb41417a31de3c1be495cc8bd607609c04229b
mmeiser <mmeiser19@gmail.com>
2023-01-29 19:07:43 -0500

    finished 2, 6, 8, working on 9 and 12

--- Labs/processapilab/q6.c
+++ Labs/processapilab/q6.c
@@ -1,11 +1,21 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <unistd.h>
-#include <string.h>
-#include <fcntl.h>
-#include <assert.h>
 #include <sys/wait.h>
+#include <stdlib.h>
 
 int main() {
-    return 0;
+    int rc = fork();
+
+    if (rc == 0) { // child
+        printf("I am child process. My PID is %d\n", getpid());
+        exit(0);
+    }
+    else if (rc > 0) { // parent
+        int status;
+        waitpid(rc, &status, 0);
+        printf("I am parent process. My PID  is %d\n", getpid());
+    }
+    else {
+        fprintf(stderr, "Error occurred during fork()");
+    }
 }
