d5ced9cb4d407549235e332cb9a6eadcc976aac2
megan <mmeiser19@gmail.com>
2023-01-29 19:01:41 -0500

    Working on process API lab

---
+++ Labs/processapilab/q2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <fcntl.h>
+#include <assert.h>
+#include <sys/wait.h>
+
+int main() {
+    // write a program that that opens a file and then calls fork to create a new process
+
+    return 0;
+}
---
+++ Labs/processapilab/q9.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <fcntl.h>
+#include <assert.h>
+#include <sys/wait.h>
+
+int main() {
+
+    return 0;
+}
